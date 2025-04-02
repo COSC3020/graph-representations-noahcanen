3ba4d7b2db9a6e959df621b144d8780d8dcfaa24
noahcanen <ncanen@uwyo.edu>
2025-04-02 15:59:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    length = adjMatrix.length
+    AdjList = new Array(length)
+    for(let x = 0 ; x < length ; x++){
+        edges = []
+        for(let y = 0 ; y < length ; y++){
+            if(adjMatrix[x][y] >= 1){
+                edges.push(y)
+            }
+        }
+        AdjList[x] =edges
+    }
+    return AdjList;
 }
